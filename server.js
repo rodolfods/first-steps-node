34a81b39a7c61b19017314e8bd4f1a548d21e1a3
rodolfo.ds <rodolfo-dartora@hotmail.com>
2019-06-10 11:18:56 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const server = express();
+const path = require('path');
+const router = express.Router();
+
+const app = require("./app");
+/*
+server.get('/', function (req, res) {
+    res.send(index);
+});
+*/
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+server.get('/download', function (req, res) {
+
+    app.execute(res).then((fileNameZip) => {
+        // res.send(`todas imagens baixadas ${fileNameZip}`);
+        const file = `${__dirname}/${fileNameZip}`;
+        console.log("downloading file", file);
+        res.download(file);
+    });
+
+});
+
+server.use('/', router);
+
+server.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
